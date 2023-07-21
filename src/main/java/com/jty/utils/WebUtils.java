1395254151aeef1648170fccb44340db377aabb1
jty <642096532@qq.com>
2023-07-22 03:39:30 +0800

    JwtUtil、WebUtils导入

---
+++ src/main/java/com/jty/utils/WebUtils.java
@@ -0,0 +1,33 @@
+package com.jty.utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * TODO Web工具
+ *
+ * @author ss_419
+ * @version 1.0
+ * @date 2023/3/3 10:39
+ */
+public class WebUtils {
+    /**
+     * 将字符串渲染到客户端
+     *
+     * @param response 渲染对象
+     * @param string   待渲染的字符串
+     * @return null
+     */
+    public static String renderString(HttpServletResponse response, String string) {
+        try {
+            response.setStatus(200);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            response.getWriter().print(string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
