9eba43ee846810fba054327c4f972d5236d717d4
jty <642096532@qq.com>
2023-07-22 03:19:35 +0800

    security、redis依赖导入 RedisConfig 配置序列化

--- src/main/java/com/jty/JtyFrameworkApplication.java
+++ src/main/java/com/jty/JtyFrameworkApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JtyFrameworkApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(JtyFrameworkApplication.class, args);
     }
